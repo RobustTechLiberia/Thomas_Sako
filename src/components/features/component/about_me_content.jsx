7a601532edbdee7ae80ab4600d5901ffcc44c4b1
Gabriel_31997 <gabrielwkun@gmail.com>
2026-08-18 15:19:38 +0000

    deploy

--- src/components/features/component/about_me_content.jsx
+++ src/components/features/component/about_me_content.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../../../images/Copilot_20260816_124134.png";
+// import profile from "../../../images/Copilot_20260816_124134.png";
 import OurTeam from "../../../components/features/component/our_team";
 import "@fortawesome/fontawesome-free/css/all.min.css"; // Import Font Awesome
 
@@ -7,9 +7,19 @@ class AboutContent extends React.Component {
   render() {
     return (
       <>
-        <div className="bg-white md:bg-violet-950 lg:bg-violet-950 gap-5 md:gap-10 lg:gap-10 h-auto md:h-140 lg:h-140 mt-0 md:mt-8 lg:mt-8 mx-0 md:mx-20 lg:mx-20">
+        <div className="w-full md:h-150 lg:h-150 md:px-20 md:mt-10 lg:mt-10 lg:px-20 px-2 mt-2 h-auto bg-white overflow-hidden relative shadow-none">
+          <iframe
+            className="w-full aspect-video border-none" // Makes it act like a responsive fluid image
+            src="https://www.youtube.com/embed/G9cl0kgd8Q4?si=PKpP5gIdk8t9iS5j"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+        {/* <div className="bg-white md:bg-violet-950 lg:bg-violet-950 gap-5 md:gap-10 lg:gap-10 h-auto md:h-140 lg:h-140 mt-0 md:mt-8 lg:mt-8 mx-0 md:mx-20 lg:mx-20">
           <div className="flex flex-wrap justify-center items-start md:justify-start lg:justify-start h-auto md:mx-20 lg:mx-20 md:pt-6 lg:pt-6">
-            {/* Profile image */}
+          
             <div className="w-full md:w-auto lg:w-auto flex justify-center md:justify-start lg:justify-start mt-5 md:mt-8 lg:mt-8">
               <img
                 src={profile}
@@ -18,7 +28,7 @@ class AboutContent extends React.Component {
               />
             </div>
 
-            {/* Auto bio */}
+            
             <div className="w-full md:w-md lg:w-md text-center md:text-left md:mx-18 lg:mx-18 mt-5 md:mt-8 lg:mt-8 flex flex-col justify-center md:h-80 lg:h-80">
               <h1 className="font-sans font-semibold capitalize text-5xl md:text-white lg:text-white text-violet-950">
                 thomas sako
@@ -27,30 +37,30 @@ class AboutContent extends React.Component {
                 host
               </span>
 
-              {/* Social icons */}
+             
               <div className="flex justify-center md:justify-start lg:justify-start md:items-start lg:items-start gap-5 mt-2">
-                {/* YouTube */}
+               
                 <div className="p-3 rounded-full bg-transparent border-none border-white transition">
                   <a href="#" className="cursor-pointer">
                     <i className="fa-brands fa-youtube md:text-2xl lg:text-2xl text-xl md:text-white lg:text-white text-violet-900"></i>
                   </a>
                 </div>
 
-                {/* X (Twitter) */}
+              
                 <div className="p-3 rounded-full bg-transparent border-none border-white transition">
                   <a href="#" className="cursor-pointer">
                     <i className="fa-brands fa-x-twitter md:text-2xl lg:text-2xl text-xl md:text-white lg:text-white text-violet-900"></i>
                   </a>
                 </div>
 
-                {/* Facebook */}
+                
                 <div className="p-3 rounded-full bg-transparent border-none border-whit transition">
                   <a href="#" className="cursor-pointer">
                     <i className="fa-brands fa-facebook-f md:text-2xl lg:text-2xl text-xl md:text-white lg:text-white text-violet-900"></i>
                   </a>{" "}
                 </div>
 
-                {/* Instagram */}
+                
                 <div className="p-3 rounded-full bg-transparent border-none border-white transition">
                   <a href="#" className="cursor-pointer">
                     <i className="fa-brands fa-instagram md:text-2xl lg:text-2xl text-xl md:text-white lg:text-white text-violet-900"></i>{" "}
@@ -66,7 +76,7 @@ class AboutContent extends React.Component {
               </p>
             </div>
           </div>
-        </div>
+        </div> */}
         <OurTeam />
       </>
     );
