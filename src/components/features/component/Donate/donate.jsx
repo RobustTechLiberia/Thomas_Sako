12b10e16d3c2fa6962c757c3161f3bd8daab82df
Gabriel_31997 <gabrielwkun@gmail.com>
2026-09-02 03:31:26 +0000

    deploy

--- src/components/features/component/Donate/donate.jsx
+++ src/components/features/component/Donate/donate.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-// donation form
 import DonateForm from "../Donate/form";
 
 class Donate extends React.Component {
   render() {
     return (
       <>
-<div className="flex flex-wrap justify-center items-center md:mt-32 lg:py-32 mt-28 py-20 bg-white md:w-80 lg:w-80 w-auto h-96 shadow-xs">
-    <DonateForm/>
-</div>
+        <div className="flex flex-wrap justify-center items-center md:mt-32 lg:py-32 mt-28 py-20 bg-white md:w-140 lg:w-160 w-full min-h-96 shadow-sm rounded-lg">
+          <DonateForm />
+        </div>
       </>
     );
   }
