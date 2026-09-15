c699fdbe972400511d91edc98400c2ec3c93d871
Gabriel_31997 <gabrielwkun@gmail.com>
2026-09-15 22:22:37 +0000

    deploy

--- server/app.js
+++ server/app.js
@@ -3,24 +3,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-const port = 8080;
 const app = express();
 
-// external routers
 const subscribeRouter = require("./routes/subscribe");
 const databaseRouter = require("./routes/db");
 const questionRouter = require("./routes/question");
 const SocialRouter = require("./routes/socialmedia");
 
-//question router mounted
+// express json middleware
+app.use(express.json());
+
+// question router mounted
 app.use("/question", questionRouter);
 
 // social media router
 app.use(SocialRouter);
 
-app.use(express.json());
-
+// external endpoints
 const allowedOrigins = [
   "http://localhost:5173",
   "https://your-frontend-domain.com",
@@ -41,34 +40,9 @@ app.use(
 app.use("/", subscribeRouter);
 app.use("/", databaseRouter);
 
+// default route
 app.get("/home", (req, res) => {
   res.send("hello, world!");
 });
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get("/", (req, res, next) => {
-  if (
-    req.path.startsWith("/db") ||
-    req.path.startsWith("/subscribe") ||
-    req.path.startsWith("/home") ||
-    req.path.startsWith("/results")
-  ) {
-    return next();
-  }
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"), (err) => {
-    if (err) {
-      res.status(404).json({
-        error: "Client build files not compiled yet. Run npm run build.",
-      });
-    }
-  });
-});
-
-app.listen(port, (err) => {
-  if (!err) {
-    console.log(`server is running on port ${port}`);
-  } else {
-    console.log(`server crash at ${port}`);
-  }
-});
+module.exports = app;
