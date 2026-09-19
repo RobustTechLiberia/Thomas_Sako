85117d534355d7f8c9d3405802287a061f21cf9f
Gabriel_31997 <gabrielwkun@gmail.com>
2026-09-19 11:38:47 +0000

    deployment

--- src/components/features/component/guest_host.jsx
+++ src/components/features/component/guest_host.jsx
@@ -6,7 +6,7 @@ class GuestHost extends React.Component {
     return (
       <>
         <div className="md:h-200 lg:h-200 bg-white">
-          <h1 className="md:text-5xl lg:text-5xl md:mx-32 lg:mx-32 mx-10 md:mt-20 lg:mt-20 mt-10 text-4xl font-black text-violet-800 uppercase">
+          <h1 className="md:text-5xl lg:text-5xl md:mx-32 lg:mx-32 mx-10 md:mt-20 lg:mt-20 mt-10 text-4xl font-black text-[#30497D] uppercase">
             guest host
           </h1>
           <GuestGallery />
