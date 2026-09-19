a8a3b970c8ba135fc09d05b546ee97369470d6cb
Gabriel_31997 <gabrielwkun@gmail.com>
2026-09-19 14:53:44 +0000

    deployment

--- server/routes/db.js
+++ server/routes/db.js
@@ -11,6 +11,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 };
 
 const handleVoteInsertion = (req, res) => {
