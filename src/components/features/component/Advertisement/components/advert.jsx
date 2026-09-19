2960309c8cab785fffaace781d1d08fc17a3c1db
Gabriel_31997 <gabrielwkun@gmail.com>
2026-09-19 14:09:48 +0000

    deployment

--- src/components/features/component/Advertisement/components/advert.jsx
+++ src/components/features/component/Advertisement/components/advert.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import advert from "../../../../../images/Advertising-2.png";
+import AdSenseWidget from "./AdSenseWidget";
+// import advert from "../../../../../images/Advertising-2.png";
 
 class Advert extends React.Component {
   render() {
     return (
       <>
         <div className="flex flex-col justify-center items-center object-cover border w-80 h-auto mb-20 md:mb-0 lg:mb-0 border-gray-400">
-          <img
-            src={advert}
-            alt=""
-            srcset=""
-            className="w-auto h-auto object-cover"
-          />
+          <AdSenseWidget client="ca-pub-XXXXXXXXXXXXXXXX" slot="YYYYYYYYYY" />
           <div className="flex flex-col justify-center items-center w-full bg-[#253C6D]">
             <h1 className="text-center text-white font-semibold text-lg px-5 py-5 uppercase">
               advertisement
